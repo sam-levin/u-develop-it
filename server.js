09aa47abdd3978de959c96e0ac0a44817147565f
sam-levin <sam.ky.levin@gmail.com>
2022-04-21 16:26:56 -0700

    building database calls

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require ('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,6 +6,19 @@ const app = express();
 // express middleware
 app.use (express.urlencoded({ extended: false}));
 app.use (express.json());
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'PENIS10!!!',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+)
+
+db.query('SELECT * FROM candidates', (err, rows) => {
+    console.log(rows);
+})
 app.use((req, res) => {
     res.status(404).end();
 });
